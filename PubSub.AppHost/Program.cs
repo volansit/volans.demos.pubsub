997425645556fc7ee6aeb6cbf4a1ef742de143da
Shahzad Butt, Ali (EXT) <ali.shahzad.butt@ssab.com>
2025-07-31 22:45:26 +0200

    Add project files.

---
+++ PubSub.AppHost/Program.cs
@@ -0,0 +1,27 @@
+using Aspire.Hosting;
+using Projects;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var rabbitmqPassword = builder.AddParameter("rabbitmq-password", secret: true);
+
+var rabbitMq = builder.AddRabbitMQ("eventbus", password: rabbitmqPassword)
+    .WithManagementPlugin()
+    .WithLifetime(ContainerLifetime.Persistent);
+
+builder.AddProject<RabbitConsumer>("RabbitMQConsumer")
+    .WithReference(rabbitMq).WaitFor(rabbitMq);
+/*
+ * removed pre added references
+var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.PubSub_ApiService>("apiservice");
+
+builder.AddProject<Projects.PubSub_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithReference(apiService)
+    .WaitFor(apiService);
+*/
+builder.Build().Run();
---
+++ RabbitConsumer/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Hello, World!");
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.AddServiceDefaults();
+builder.AddRabbitMQClient("eventbus");
+
+builder.Build().Run();
