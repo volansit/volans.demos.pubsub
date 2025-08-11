212e5c13fd5669403e77418ca69a4967e94df9db
Shahzad Butt, Ali (EXT) <ali.shahzad.butt@ssab.com>
2025-08-11 14:29:56 +0200

    added comments a WaitFor apiservice and ex URLs in readme

--- PubSub.AppHost/Program.cs
+++ PubSub.AppHost/Program.cs
@@ -1,27 +1,49 @@
 using Aspire.Hosting;
 using Projects;
 
+//docs/ examples:
+// https://learn.microsoft.com/en-us/dotnet/aspire/messaging/rabbitmq-integration?tabs=dotnet-cli
+// 
 var builder = DistributedApplication.CreateBuilder(args);
 
 var rabbitmqPassword = builder.AddParameter("rabbitmq-password", secret: true);
 
+/*
+ * add a RabbitMQ server resource using builder.AddRabbitMQ("resourceName"). 
+ * The "resourceName" will be used as the connection string name when referencing it in other projects. * 
+ */ 
 var rabbitMq = builder.AddRabbitMQ("eventbus", password: rabbitmqPassword)
     .WithManagementPlugin()
     .WithLifetime(ContainerLifetime.Persistent);
 
-var apiService = builder.AddProject<Projects.PubSub_ApiService>("apiservice")
+/*
+ * WithReference:
+ * Injects a connection string as an environment variable from the source resource
+ * into the destination resource, using the source resource's name as the connection
+ * string name (if not overridden). The format of the environment variable will
+ * be "ConnectionStrings__{sourceResourceName}={connectionString}
+ */
+var apiSerivce = builder.AddProject<PubSub_ApiService>("apiservice")
     .WithReference(rabbitMq);
 
+/*
+ * Waits for the dependency reeource to enter the "Running" state, before starting the resource
+ */
 builder.AddProject<RabbitConsumer>("RabbitMQConsumer")
-    .WithReference(rabbitMq).WaitFor(rabbitMq);
+    .WithReference(rabbitMq)
+    .WaitFor(rabbitMq)
+    .WaitFor(apiSerivce);
+
 
 builder.AddProject<RabbitSubscriber>("RabbitSubscriber1")
-    .WithReference(rabbitMq).WaitFor(rabbitMq);
+    .WithReference(rabbitMq)
+    .WaitFor(rabbitMq)
+    .WaitFor(apiSerivce);
 
 builder.AddProject<RabbitSubscriber>("RabbitSubscriber2")
-    .WithReference(rabbitMq).WaitFor(rabbitMq);
-
-
+    .WithReference(rabbitMq)
+    .WaitFor(rabbitMq)
+    .WaitFor(apiSerivce);
 /*
  * removed pre added references
 var cache = builder.AddRedis("cache");
--- README.md
+++ README.md
@@ -13,10 +13,12 @@ Ctr+F5
 You should get the .Net Aspire dashboard. The dashboard will list all the resources that are part of this application including eventbus, RabbitMQConsumer and the PubSub.APIService (the producer of our messages).
 
 - To be send a message, use your browser and go to the URL: https://localhost:PORT/send-message. You can get the port nr from Aspire dashboard, eg: 7367.
+	So ex: https://localhost:7367/send-message
 Above will send a message to the "order" queue.
 The RabbitMQConsumer is consumer of the message and so you can observe the messaging coming to the rabbitMQ queue via RabbitMQ's management portal. 
 Further one can also go to the RabbitMQConsumer's console log to inspect the incoming messages.
 
 - To be publish a message, use your browser and go to the URL: https://localhost:PORT/publish-message. You can get the port nr from Aspire dashboard, eg: 7367.
 the message is subscribed by two subscribers. you can confirm that message is received by two subscribers from the Aspire dashboard.
+	so ex: https://localhost:7367/publish-message
 
